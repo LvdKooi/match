9dd3eac1ef06f025824e4d39f540d55935009ae2
lvdkooi <lvvdkooi@hotmail.com>
2023-05-11 09:55:40 +0200

    Clean up

--- src/main/java/nl/kooi/match/infrastructure/TeamPersistenceService.java
+++ src/main/java/nl/kooi/match/infrastructure/TeamPersistenceService.java
@@ -3,7 +3,6 @@ package nl.kooi.match.infrastructure;
 
 import lombok.RequiredArgsConstructor;
 import nl.kooi.match.core.domain.Team;
-import nl.kooi.match.infrastructure.entity.TeamEntity;
 import nl.kooi.match.infrastructure.mapper.Mapper;
 import nl.kooi.match.infrastructure.port.TeamDao;
 import nl.kooi.match.infrastructure.repository.TeamRepository;
@@ -11,8 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -27,13 +24,4 @@ public class TeamPersistenceService implements TeamDao {
         return teamRepository.findById(teamId).map(mapper::map);
     }
 
-    @Override
-    public boolean isPlayerPartOfTeams(Long playerId, String team1, String team2) {
-        return Stream.of(teamRepository.findByName(team1), teamRepository.findByName(team2))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .map(TeamEntity::getPlayers)
-                .flatMap(Set::stream)
-                .anyMatch(player -> player.getId().equals(playerId));
-    }
 }
